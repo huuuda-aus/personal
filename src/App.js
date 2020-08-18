ce3b2055fb59935f5d5ad5d691ba1121d26e15bf
petrh-bizcover <petrh@bizcover.com.au>
2020-08-18 11:32:15 +1000

    homepage update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-import * as indexPage from './index.html';
 
 function App() {
   return (
     <div className="App">
-      {indexPage}
+      <h1>HUUUDOVO LIVE!</h1>
     </div>
   );
 }
